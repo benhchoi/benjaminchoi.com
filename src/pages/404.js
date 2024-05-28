75f3508cb838def363000e5fdb1a20cd780a65bf
Ben Choi <choi.bc97@gmail.com>
2024-05-28 17:07:47 -0400

    Fix and update personal website

---
+++ src/pages/404.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { headData } from '../mock/data';
+import '../style/main.scss';
+
+export default () => {
+  const { lang } = headData;
+
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Page not found</title>
+        <html lang={lang || 'en'} />
+        <meta name="description" content="Page not found" />
+      </Helmet>
+    </>
+  );
+};
